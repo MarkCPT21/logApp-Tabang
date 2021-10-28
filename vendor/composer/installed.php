918f5de659c385d295b4e373d71320b0431423b7
MarkCPT21 <201980201@psu.palawan.edu.ph>
2021-10-29 02:34:57 +0800

    added composer.json and created a Heroku App

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1be1e0f9a939491d668bb3b5c61e0bd931591026',
+        'name' => 'markcpt21/logapp-tabang',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'markcpt21/logapp-tabang' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1be1e0f9a939491d668bb3b5c61e0bd931591026',
+            'dev_requirement' => false,
+        ),
+    ),
+);
